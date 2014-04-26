36d8bc05ff6de9b476b10c5659f8fd3a982e0075
hkchristian.ngo@gmail.com <hkchristian.ngo@gmail.com@400441cf-e8c6-9e20-8b28-10f667b2ff2f>
2014-04-26 19:54:59 +0000

    Add Main Menu & some prefab.

---
+++ trunk/runner-swap/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+	public bool isQuitButton = false;
+	private Color hoverColor = new Color (192/255f, 57/255f, 43/255f);
+
+	void OnMouseEnter () {
+		renderer.material.color = hoverColor;
+	}
+
+	void OnMouseExit () {
+		renderer.material.color = Color.white;
+	}
+
+	void OnMouseDown () {
+		if (isQuitButton) {
+			Application.Quit();
+		} else {
+			Application.LoadLevel("Level01");
+		}
+
+	}
+}
