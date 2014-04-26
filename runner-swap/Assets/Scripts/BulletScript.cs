be08f44dc559fa36b7ea45132193e0b0547c6afb
hkchristian.ngo <hkchristian.ngo@gmail.com>
2014-04-26 15:19:37 +0000

    Add bullet prefab

---
+++ runner-swap/Assets/Scripts/BulletScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletScript : MonoBehaviour {
+
+	public enum Type {FIRE, FROST}
+	public Type type;
+	public int speed;
+	public int maxDistance;
+
+	private float traveledDistance;
+
+	// Use this for initialization
+	void Start () {
+		traveledDistance = 0;
+		this.rigidbody.AddForce (Vector3.right * speed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		traveledDistance += speed;
+		if (traveledDistance >= maxDistance) {
+			DestroyObject(this.gameObject);
+		}
+	}
+}
---
+++ runner-swap/Assets/Scripts/ObstacleScript.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleScript : MonoBehaviour {
+
+	public enum Type {WALL, WEAKED_WALL, SPIDER_NET, POISON}
+	public Type type;
+	
+	private string collisionTag;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision collision) {		
+		collisionTag = collision.gameObject.tag;
+		switch (type) {
+			case Type.WALL:
+				WallCollision(collision);
+				break;
+			case Type.WEAKED_WALL:
+				WeakedWallCollision(collision);
+				break;
+			case Type.POISON:
+				PoisonCollision(collision);
+				break;
+		}
+	}
+
+	void WallCollision(Collision collision) {
+		if (collisionTag == "Hero" || collisionTag == "Bullet") {
+			DestroyObject(collision.gameObject);
+		}
+	}
+
+	void WeakedWallCollision(Collision collision) {
+		if (collisionTag == "Hero") {
+			//slow down hero
+		}
+		else if (collisionTag == "Bullet") {
+			DestroyObject (collision.gameObject);
+		}
+
+		DestroyObject (this.gameObject);
+	}
+
+	void PoisonCollision(Collision collision) {
+		if (collisionTag == "Hero") {
+			DestroyObject(collision.gameObject);
+		}
+	}
+}
